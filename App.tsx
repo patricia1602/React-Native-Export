b56b0302b04b105f6271d64695990d4a14f581a7
patricia1602 <patyamiga16@hotmail.com>
2025-04-11 16:21:02 -0300

    Rederizando listas

--- App.tsx
+++ App.tsx
@@ -1,43 +1,131 @@
 import { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Button,
-  TouchableOpacity,
-  Alert,
-  TextInput,
-} from "react-native";
-//import Icon from "./assets/icon.png";
-const Icon = require("./assets/icon.png");
+import { StyleSheet, View, Text, Image, FlatList } from "react-native";
+
+const ArrayUser = [
+  {
+    nome: "Patrícia",
+    funcao: "Programador",
+  },
+  {
+    nome: "Leonardo",
+    funcao: "Barbeiro",
+  },
+  {
+    nome: "Manuela",
+    funcao: "Cabeleilera",
+  },
+  {
+    nome: "Maria",
+    funcao: "Administradora",
+  },
+
+  {
+    nome: "Marcelo",
+    funcao: "Motorista",
+  },
+  {
+    nome: "Patrícia",
+    funcao: "Programador",
+  },
+  {
+    nome: "Leonardo",
+    funcao: "Barbeiro",
+  },
+  {
+    nome: "Manuela",
+    funcao: "Cabeleilera",
+  },
+  {
+    nome: "Maria",
+    funcao: "Administradora",
+  },
+  {
+    nome: "Marcelo",
+    funcao: "Motorista",
+  },
+];
 
 export default function App() {
-  const [nome, setnome] = useState("");
+  const _renderCard = () => {
+    if (ArrayUser.length == 0) {
+      return <></>;
+    }
 
- 
+    const vet: any = [];
+
+    // Renderizando uma lista
+    ArrayUser.map((item, index) => {
+      vet.push(
+        <View style={styles.card} key={index}>
+          <View style={styles.boxImg}>
+            <Image
+              resizeMode="cover"
+              style={{ width: "100%", height: "100%" }}
+              source={{ uri: `https://robohash.org/${index}.png` }}
+            />
+          </View>
+          <View style={styles.box}>
+            <Text style={styles.title}>Nome: </Text>
+            <Text style={styles.desc}>{item.nome}</Text>
+            <Text style={styles.title}>Função: </Text>
+            <Text style={styles.desc}>{item.funcao}</Text>
+          </View>
+        </View>
+      );
+    });
+
+    //for (let i = 0; i < ArrayUser.length; i++) {
+    //vet.push(
+    //<View style={styles.card} key={i}>
+    //</View><View style={styles.boxImg}>
+    ///<Image
+    //resizeMode="cover"
+    //style={{ width: "100%", height: "100%" }}
+    //source={{ uri: `https://robohash.org/${i}.png` }}
+    ///>
+    //</View>
+    //<View style={styles.box}>
+    //<Text style={styles.title}>Nome: </Text>
+    //<Text style={styles.desc}>{ArrayUser[i].nome}</Text>
+    //<Text style={styles.title}>Função: </Text>
+    //<Text style={styles.desc}>{ArrayUser[i].funcao}</Text>
+    //</View>
+    //</View>
+    //);
+    //}
+
+    return vet;
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.box1}>
-        <Text>Título</Text>
-        <Text>Descrição, esse app é muito bom!</Text>
-        <Image source={Icon} style={{ width: 100, height: 100, marginTop: 10 }} />
-        <Button title="Ok" onPress={() => Alert.alert("Olá mundo!")} />
-        
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => Alert.alert("Seu nome é: " + nome)}
-        >
-          <Text>OK2</Text>
-        </TouchableOpacity>
-
-        <Text>Digite o seu nome:</Text>
-        <TextInput
-          value={nome}
-          style={styles.input}
-          onChangeText={(text) => setnome(text)} // Correção aqui
-        />
-      </View>
+      {/*<ScrolView>
+    _renderCard()
+    </ScrolView>*/}
+      
+      <FlatList
+        data={ArrayUser}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => {
+          return (
+            <View style={styles.card}>
+              <View style={styles.boxImg}>
+                <Image
+                  resizeMode="cover"
+                  style={{ width: "100%", height: "100%" }}
+                  source={{ uri: `https://robohash.org/${index}.png` }}
+                />
+              </View>
+              <View style={styles.box}>
+                <Text style={styles.title}>Nome: </Text>
+                <Text style={styles.desc}>{item.nome}</Text>
+                <Text style={styles.title}>Função: </Text>
+                <Text style={styles.desc}>{item.funcao}</Text>
+              </View>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
@@ -45,33 +133,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    padding: 20,
-  },
-  box1: {
-    width: "100%",
-    height: 200,
     backgroundColor: "#dedede",
-    padding: 10,
-  },
-  box2: {
-    width: "100%",
-    height: 200,
-    backgroundColor: "red", // Correção na cor
-  },
-  button: {
-    width: 100,
-    height: 100,
-    backgroundColor: "red",
     alignItems: "center",
-    justifyContent: "center",
-    marginTop: 20,
+    paddingVertical: 60,
+    paddingHorizontal: 40,
   },
-  input: {
+  card: {
     width: "100%",
-    height: 40,
-    backgroundColor: "gray",
+    height: 140,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 20,
+    flexDirection: "row",
     marginTop: 10,
-    borderRadius: 4,
+  },
+  box: {
+    //borderWidth: 1,
+    width: "70%",
+    height: "100%",
+    shadowColor: "#ffff",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16,
+
+    elevation: 24,
+  },
+  boxImg: {
+    width: "30%",
+    height: "100%",
+    //borderWidth: 1,
+  },
+  title: {
+    color: "gray",
+  },
+  desc: {
+    fontFamily: "bold",
   },
 });
